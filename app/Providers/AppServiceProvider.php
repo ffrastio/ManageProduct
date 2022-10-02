5f52f82e6aa8411ce2c6e198fbf23b43519b1242
fani frastio <fanitl2a@gmail.com>
2022-10-02 13:09:46 +0700

    currency rupiah

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Blade::directive('rupiah', function ($str) {
+            return "Rp. <?php echo number_format($str, 0,',','.') ?>";
+        });
     }
 }
